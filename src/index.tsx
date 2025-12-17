f01a550d7d0e2e1d7abf4a2b5382f56afc6b966c
Alex <mntokhirjonov@gmail.com>
2025-12-17 12:17:54 +0500

    feat: MUI standard and customized integrations

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
-import "../src/css/index.css"
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
+import CssBaseline from "@mui/material/CssBaseline";
+import "../src/css/index.css"
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -12,7 +15,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
