769d64ecc2d8b699d7865046ca8845bd9a5d9afc
Alex <mntokhirjonov@gmail.com>
2025-12-18 19:19:29 +0500

    feat: develop VehiclesPage nested routing system

---
+++ src/app/screens/vehiclesPage/ChosenVehicle.tsx
@@ -0,0 +1,3 @@
+export default function ChosenVehicle() {
+    return <div>ChosenVehicles</div>
+}
--- src/app/screens/vehiclesPage/index.tsx
+++ src/app/screens/vehiclesPage/index.tsx
@@ -1,6 +1,21 @@
-import { Container } from "@mui/material";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import ChosenVehicle from "./ChosenVehicle";
+import Vehicles from "./Vehicles";
 
 
-export function VehiclesPage() {
-  return <Container>VehiclesPage</Container>
+export default function VehiclesPage() {
+  const vehicles = useRouteMatch();
+
+  return (
+    <div className="vehicles-page">
+      <Switch>
+        <Route path={`${vehicles.path}/:vehicleId`}>
+          <ChosenVehicle />
+        </Route>
+        <Route path={`${vehicles.path}`}>
+          <Vehicles />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
