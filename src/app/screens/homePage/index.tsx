e9dacd23e6e5ea2206465424b29569ae9b398a19
Alex <mntokhirjonov@gmail.com>
2025-12-29 09:59:34 +0500

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import "../../../css/home.css";
+import React, { useEffect } from "react";
 import BrandRow from "./BrandRow";
 import { MainHome } from "./MainHome";
 import PopularVehicles from "./PopularVehicles";
@@ -7,9 +6,35 @@ import AboutUs from "./AboutUs";
 import { HowItWorks } from "./HowItWorks";
 import { Events } from "./Events";
 import { MobileAppDownload } from "./MobileDownload";
-import { Footer } from "../../components/footer";
+
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularVehicles } from "./slice";
+import { Vehicle } from "../../../lib/types/vehicle";
+import { retrievePopularVehicles } from "./selector";
+import "../../../css/home.css";
+
+/** REDUX SLICE & SELECTOR */
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularVehicles: (data: Vehicle[]) => dispatch(setPopularVehicles(data)),
+});
+
+const popularVehiclesRetriever = createSelector(
+  retrievePopularVehicles,
+  (popularVehicles) => ({ popularVehicles })
+);
 
 export default function HomePage () {
+  const { setPopularVehicles } = actionDispatch(useDispatch());
+  const { popularVehicles } = useSelector(popularVehiclesRetriever);
+  // Selector: Store => Data
+
+  useEffect(() => {
+    // Backend server data request => Data
+    // Slice: Data => Store
+  }, []);
+
   return <div className="homepage">
     <MainHome />
     <HowItWorks />
